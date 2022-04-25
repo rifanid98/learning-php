42827884f40dd49661b36bd06823d9a5de912cbb
Adnin Rifandi Sutanto Putra <adninsijawa.office@gmail.com>
2022-04-25 17:12:55 +0700

    hello world

---
+++ php01_fundamental/lesson02_hello-world/hello_world.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * File PHP
+ *
+ * - File kode program PHP diakhiri dengan extention .php
+ * - Di awal kode program PHP, wajib menambahkan <?php. Dan diakhir kode program php perlu ditambahkan ?> (tapi tidak wajib)
+ * - Nama file PHP tidak ada aturan, jadi kita bisa membuat file PHP seperti membuat file lain biasanya. Namun agar mempermudah saat menjalankan file PHP, direkomendasikan tidak menggunakan spasi.
+ * - Untuk menampilkan tulisan di PHP, kita bisa menggunakan perintah echo
+ */
+
+echo "Hello World";
